5ad76ecfbcb72622b9e5ebde4621b5df6c13be2b
shibdib <supernfadmin@gmail.com>
2019-02-02 06:06:15 -0500

    debugging

--- classes/ranger.js
+++ classes/ranger.js
@@ -1,4 +1,5 @@
 game_log("---Ranger Script Start---");
+send_party_request('Shibtank')
 //Put monsters you want to kill in here
 //If your character has no target, it will travel to a spawn of the first monster in the list below.
 let state = "farm";
