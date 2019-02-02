7980ee445333fe6e13a6f25b02d76980c5320a36
shibdib <supernfadmin@gmail.com>
2019-02-02 05:40:09 -0500

    debugging

--- classes/priest.js
+++ classes/priest.js
@@ -47,38 +47,6 @@ function farm() {
     }
 }
 
-//This function will ether move straight towards the target entity,
-//or utilize smart_move to find their way there.
-function move_to_target(target) {
-    if (can_move_to(target.real_x, target.real_y)) {
-        smart.moving = false;
-        smart.searching = false;
-        move(
-            character.real_x + (target.real_x - character.real_x) / 2,
-            character.real_y + (target.real_y - character.real_y) / 2
-        );
-    }
-    else {
-        if (!smart.moving) {
-            smart_move({x: target.real_x, y: target.real_y});
-        }
-    }
-}
-
-//This function will ether move straight towards the target entity,
-//or utilize smart_move to find their way there.
-function move_to_position(position) {
-    if (can_move_to(position.x, position.y)) {
-        smart.moving = false;
-        smart.searching = false;
-        move(
-            character.real_x + (position.x - character.real_x) / 2,
-            character.real_y + (position.y - character.real_y) / 2
-        );
-    }
-    else {
-        if (!smart.moving) {
-            smart_move({x: position.x, y: position.y});
-        }
-    }
+function on_party_invite(name){
+    if (name === 'Shibtank') accept_party_invite(name);
 }
--- helpers/party.js
+++ helpers/party.js
@@ -1,7 +1,3 @@
 function on_party_request(name){
     accept_party_request(name);
 }
-
-function on_party_invite(name){
-    if (name === 'Shibtank') accept_party_invite(name);
-}
