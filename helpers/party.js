5371265662621dcaf827d06b49919f813b70c159
shibdib <supernfadmin@gmail.com>
2019-02-02 04:39:52 -0500

    debugging

--- helpers/party.js
+++ helpers/party.js
@@ -1,8 +1,6 @@
 function on_party_request(name){
-    if(member.party_logic.party_list.includes(name))
-        accept_party_request(name);
+    accept_party_request(name);
 }
 function on_party_invite(name){
-    if(member.party_logic.party_list.includes(name))
-        accept_party_invite(name);
+    accept_party_invite(name);
 }
