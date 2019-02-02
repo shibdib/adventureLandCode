5e43c4fc246d54b8b5afc04d5b0eefc69930ec5f
shibdib <supernfadmin@gmail.com>
2019-02-01 20:27:55 -0500

    initial

---
+++ constants.js
@@ -0,0 +1,9 @@
+var Characters;
+(function (Characters) {
+    Characters["Warrior"] = "Shibtank";
+    Characters["Mage"] = "Shibdib";
+    Characters["Ranger"] = "Shibtard";
+    Characters["Rogue"] = "Shibgank";
+    Characters["Priest"] = "Shibheal";
+    Characters["Merchant"] = "Shibmerch";
+})(Characters || (Characters = {}));
---
+++ main.js
@@ -0,0 +1,21 @@
+let urls = ["https://raw.githubusercontent.com/shibdib/adventureLandCode/master/helpers/shopping.js"];
+urls.push("https://raw.githubusercontent.com/shibdib/adventureLandCode/master/classes/" + character.ctype + ".js");
+urls.forEach((u) => loadURLs(u));
+load_code(constants.js)
+
+function loadURLs( url ) {
+    var ajax = new XMLHttpRequest();
+    ajax.open( 'GET', url, false ); // <-- the 'false' makes it synchronous
+    ajax.onreadystatechange = function () {
+        var script = ajax.response || ajax.responseText;
+        if (ajax.readyState === 4) {
+            switch( ajax.status) {
+                case 200:
+                    eval.apply( window, [script] );
+                    break;
+                default:
+            }
+        }
+    };
+    ajax.send(null);
+}
