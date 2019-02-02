b6973fe45268df8adfb14dc7c4e24c70c4d4f399
shibdib <supernfadmin@gmail.com>
2019-02-02 05:11:18 -0500

    debugging

--- classes/priest.js
+++ classes/priest.js
@@ -11,7 +11,6 @@ setInterval(function () {
 
 //Potions and state
 setInterval(function () {
-    on_party_invite('Shibtank');
     state_controller();
     //Heal With Potions if we're below 75% hp.
     if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
