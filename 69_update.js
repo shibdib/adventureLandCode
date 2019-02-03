7b6560ec8b65f070bf49a36731be0792741703d4
shibdib <supernfadmin@gmail.com>
2019-02-03 11:56:43 -0500

    update is in the code

---
+++ 69_update.js
@@ -0,0 +1,60 @@
+/*
+ * @author	D4ddy-LiLd4rk
+ * @source	https://github.com/D4ddy-LiLd4rk/AdventureLand
+ */
+
+const baseURL = "https://raw.githubusercontent.com/shibdib/adventureLandCode/master/";
+
+const allFiles = [
+    "2_requires.js",
+    "80_constants.js",
+    "98_references.js",
+    "99_helpers.js",
+    "21_movement.js",
+    "22_party.js",
+    "23_shopping.js",
+    "24_combat.js",
+    "10_priest.js",
+    "11_ranger.js",
+    "12_warrior.js",
+    "13_mage.js",
+    "69_update.js"
+];
+
+
+function updateCode() {
+    parent.api_call("list_codes", {
+        callback: function () {
+            game_log("Updating from GitHub...");
+            for (let file of allFiles) {
+                let request = new XMLHttpRequest();
+                request.open("GET", baseURL + file);
+                request.onreadystatechange = function () {
+                    if (request.readyState === 4 && request.status === 200) {
+                        let codeObject = getCodeObject(file);
+                        let data = {
+                            name: codeObject.name,
+                            slot: codeObject.slot,
+                            code: request.responseText
+                        };
+                        parent.api_call("save_code", data);
+                    }
+                };
+                request.send();
+            }
+        }
+    });
+}
+
+function getCodeObject(file) {
+    let codeObject;
+
+    let arr = file.substring(0, file.length - 3).split("_");
+
+    codeObject = {
+        slot: arr[0],
+        name: arr[1]
+    };
+
+    return codeObject;
+}
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
+//Handle github update
 let update = true;
+if (update) {
+    load_code(69);
+    updateCode();
+}
 load_code(80);
-
-//Load party
-let pve_characters = [
-    {'name': 'Shibtank', 'class': 'warrior', 'slot': 12},
-    {'name': 'Shibdib', 'class': 'mage', 'slot': 13},
-    {'name': 'Shibheal', 'class': 'priest', 'slot': 10}
-];
 //Update
 if (update) {
     updateCode();
@@ -32,63 +30,3 @@ setInterval(function () {
         send_party_invite(char.name);
     }
 }, 12400);
-
-/*
- * @author	D4ddy-LiLd4rk
- * @source	https://github.com/D4ddy-LiLd4rk/AdventureLand
- */
-
-const baseURL = "https://raw.githubusercontent.com/shibdib/adventureLandCode/master/";
-
-const allFiles = [
-    "2_requires.js",
-    "80_constants.js",
-    "98_references.js",
-    "99_helpers.js",
-    "21_movement.js",
-    "22_party.js",
-    "23_shopping.js",
-    "24_combat.js",
-    "10_priest.js",
-    "11_ranger.js",
-    "12_warrior.js",
-    "13_mage.js"
-];
-
-
-function updateCode() {
-    parent.api_call("list_codes", {
-        callback: function () {
-            game_log("Updating from GitHub...");
-            for (let file of allFiles) {
-                let request = new XMLHttpRequest();
-                request.open("GET", baseURL + file);
-                request.onreadystatechange = function () {
-                    if (request.readyState === 4 && request.status === 200) {
-                        let codeObject = getCodeObject(file);
-                        let data = {
-                            name: codeObject.name,
-                            slot: codeObject.slot,
-                            code: request.responseText
-                        };
-                        parent.api_call("save_code", data);
-                    }
-                };
-                request.send();
-            }
-        }
-    });
-}
-
-function getCodeObject(file) {
-    let codeObject;
-
-    let arr = file.substring(0, file.length - 3).split("_");
-
-    codeObject = {
-        slot: arr[0],
-        name: arr[1]
-    };
-
-    return codeObject;
-}
