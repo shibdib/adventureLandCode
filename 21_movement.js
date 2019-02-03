d7149a4c7d1b15c11309abfacdc46081b7d219ed
shibdib <supernfadmin@gmail.com>
2019-02-03 11:01:47 -0500

    use move function

--- 21_movement.js
+++ 21_movement.js
@@ -3,33 +3,11 @@
 //This function will ether move straight towards the target entity,
 //or utilize smart_move to find their way there.
 function move_to_target(target) {
-    if (can_move_to(target.real_x, target.real_y) && distance_to_point(target.real_x, target.real_y)) {
-        smart.moving = false;
-        smart.searching = false;
-        move(
-            character.real_x + (target.real_x - character.real_x) / 2,
-            character.real_y + (target.real_y - character.real_y) / 2
-        );
-    } else {
-        if (!smart.moving) {
-            smart_move({x: target.real_x, y: target.real_y});
-        }
-    }
+    xmove(target.real_x, target.real_y)
 }
 
 //This function will ether move straight towards the target entity,
 //or utilize smart_move to find their way there.
 function move_to_position(position) {
-    if (can_move_to(position.x, position.y) && distance_to_point(position.real_x, position.real_y)) {
-        smart.moving = false;
-        smart.searching = false;
-        move(
-            character.real_x + (position.x - character.real_x) / 2,
-            character.real_y + (position.y - character.real_y) / 2
-        );
-    } else {
-        if (!smart.moving) {
-            smart_move({x: position.x, y: position.y});
-        }
-    }
+    xmove(position.x, position.y)
 }
