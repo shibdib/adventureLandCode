befdbcb216049db803efb7279b054e58c65edf0c
shibdib <supernfadmin@gmail.com>
2019-02-05 11:41:45 -0500

    tweak movement

--- 21_movement.js
+++ 21_movement.js
@@ -5,6 +5,8 @@ function move_to_target(target, min = 0, max = 0) {
     if (target) range = distance_to_point(target.real_x, target.real_y) + 0.1;
     // If range is good stay
     if (range && (range <= max && range >= min)) return stop();
+    // Handle close
+    if (target && (range > max || range < min || !range)) return move_to_coords(target.real_x, target.real_y);
     // If smart moving past them stop
     if (range && smart.moving && range <= character.range) return stop();
     // If moving continue
@@ -15,14 +17,16 @@ function move_to_target(target, min = 0, max = 0) {
     if (!range || !parent.entities[character.party] || range >= character.range * 4) {
         if (target) move_to_coords(target.real_x, target.real_y); else return shib_move(target);
     }
-    // Handle close
-    if (target && (range > max || range < min || !range)) move_to_coords(target.real_x, target.real_y);
 }
 
 function move_to_leader (min = 5, max = 10) {
     let leader = get_player(character.party);
     let range;
     if (leader) range = distance_to_point(leader.real_x, leader.real_y) + 0.1;
+    // Handle close
+    if (leader && (range > max || range < min || !range)) move_to_coords(leader.real_x, leader.real_y);
+    // Handle close
+    if (leader && (range > max || range < min || !range)) move_to_coords(leader.real_x, leader.real_y); else if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
     // If range is good stay
     if (range && (range <= max && range >= min)) return stop();
     // If smart moving past them stop
@@ -39,12 +43,13 @@ function move_to_leader (min = 5, max = 10) {
             return shib_move(parent.party[character.party].x, parent.party[character.party].y);
         }
     }
-    // Handle close
-    if (leader && (range > max || range < min || !range)) move_to_coords(leader.real_x, leader.real_y); else if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
+    if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
 }
 
+let coordsStoredDestination;
 function move_to_coords(x, y) {
-    if (is_moving(character)) return;
+    if (!coordsStoredDestination) coordsStoredDestination = {x: x, y: y};
+    if (is_moving(character) && coordsStoredDestination.x === x && coordsStoredDestination.y === y) return;
     if(can_move_to(x,y)) {
         if (smart.moving) stop();
         move(x,y);
@@ -52,12 +57,16 @@ function move_to_coords(x, y) {
     else smart_move({x:x,y:y});
 }
 
+let shibMoveStoredDestination;
 function shib_move(destination, y = undefined) {
-    if (is_moving(character)) return;
+    if (!shibMoveStoredDestination) shibMoveStoredDestination = destination;
+    if (is_moving(character) && shibMoveStoredDestination === destination) return;
     smart_move(destination, y);
 }
 
+let positionStoredDestination;
 function move_to_position(position) {
-    if (is_moving(character)) return;
+    if (!positionStoredDestination) positionStoredDestination = position;
+    if (is_moving(character) && positionStoredDestination.x === position.x && positionStoredDestination.y === position.y) return;
     move_to_coords(position.x, position.y)
 }
