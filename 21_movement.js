5c0f62c5ad28dd6c3fb1e966ead1cf281a38525b
shibdib <supernfadmin@gmail.com>
2019-02-04 22:00:44 -0500

    clean up movement

--- 21_movement.js
+++ 21_movement.js
@@ -13,10 +13,10 @@ function move_to_target(target, min = 0, max = 0) {
     if (parent.party[character.party].map !== character.map) return shib_move(parent.party[character.party].map);
     // Handle same map but far away
     if (!range || !parent.entities[character.party] || range >= character.range * 4) {
-        if (target) xmove(target.real_x, target.real_y); else return shib_move(target);
+        if (target) move_to_coords(target.real_x, target.real_y); else return shib_move(target);
     }
     // Handle close
-    if (target && (range > max || range < min || !range)) xmove(target.real_x, target.real_y);
+    if (target && (range > max || range < min || !range)) move_to_coords(target.real_x, target.real_y);
 }
 
 function move_to_leader (min = 5, max = 10) {
@@ -33,22 +33,26 @@ function move_to_leader (min = 5, max = 10) {
     if (parent.party[character.party].map !== character.map) return shib_move(parent.party[character.party].map);
     // Handle same map but far away
     if (!range || !parent.entities[character.party] || range >= character.range * 4) {
-        if (target) xmove(parent.party[character.party].x, parent.party[character.party].y); else return shib_move(parent.party[character.party].x, parent.party[character.party].y);
+        if (target) move_to_coords(parent.party[character.party].x, parent.party[character.party].y); else return shib_move(parent.party[character.party].x, parent.party[character.party].y);
     }
     // Handle close
-    if (leader && (range > max || range < min || !range)) xmove(leader.real_x, leader.real_y); else if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
+    if (leader && (range > max || range < min || !range)) move_to_coords(leader.real_x, leader.real_y); else if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
 }
 
 //This function will ether move straight towards the target entity,
 //or utilize smart_move to find their way there.
 function move_to_position(position) {
     if (is_moving(character)) return;
-    xmove(position.x, position.y)
+    move_to_coords(position.x, position.y)
 }
 
 function move_to_coords(x, y) {
     if (is_moving(character)) return;
-    xmove(x, y)
+    if(can_move_to(x,y)) {
+        if (smart.moving) stop();
+        move(x,y);
+    }
+    else smart_move({x:x,y:y});
 }
 
 function shib_move(destination, y = undefined) {
