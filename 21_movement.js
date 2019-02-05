deb4a211cc6654c74a2d86c69d1b01cb2cecdaa6
shibdib <supernfadmin@gmail.com>
2019-02-05 12:32:24 -0500

    revert to old move

--- 21_movement.js
+++ 21_movement.js
@@ -1,15 +1,10 @@
 //This function will ether move straight towards the target entity,
 //or utilize smart_move to find their way there.
-function move_to_target(target, min = 1, max = 2) {
+function move_to_target(target, min = 0, max = 0) {
     let range;
     if (target) range = distance_to_point(target.real_x, target.real_y) + 0.1;
     // If range is good stay
     if (range && (range <= max && range >= min)) return stop();
-    // Handle close
-    if (target && (range > max || range < min || !range)) {
-        let goodPosition = getPositionAtRange(target, min, max);
-        if (goodPosition) return move_to_coords(goodPosition.x, goodPosition.y);
-    }
     // If smart moving past them stop
     if (range && smart.moving && range <= character.range) return stop();
     // If moving continue
@@ -20,17 +15,14 @@ function move_to_target(target, min = 1, max = 2) {
     if (!range || !parent.entities[character.party] || range >= character.range * 4) {
         if (target) move_to_coords(target.real_x, target.real_y); else return shib_move(target);
     }
+    // Handle close
+    if (target && (range > max || range < min || !range)) move_to_coords(target.real_x + getRndInteger(((min + max)/2) * -1, ((min + max)/2)), target.real_y + getRndInteger(((min + max)/2) * -1, ((min + max)/2)));
 }
 
-function move_to_leader (min = 10, max = 20) {
+function move_to_leader (min = 5, max = 10) {
     let leader = get_player(character.party);
     let range;
     if (leader) range = distance_to_point(leader.real_x, leader.real_y) + 0.1;
-    // Handle close
-    if (leader && (range > max || range < min || !range)) {
-        let goodPosition = getPositionAtRange(leader, min, max);
-        if (goodPosition) return move_to_coords(goodPosition.x, goodPosition.y);
-    }
     // If range is good stay
     if (range && (range <= max && range >= min)) return stop();
     // If smart moving past them stop
@@ -41,13 +33,17 @@ function move_to_leader (min = 10, max = 20) {
     if (parent.party[character.party].map !== character.map) return shib_move(parent.party[character.party].map);
     // Handle same map but far away
     if (!range || !parent.entities[character.party] || range >= character.range * 4) {
-        if (leader) {
-            return move_to_coords(leader.x, leader.y);
-        } else {
-            return shib_move(parent.party[character.party].x, parent.party[character.party].y);
-        }
+        if (leader) move_to_coords(leader.x, leader.y); else return shib_move(parent.party[character.party].x, parent.party[character.party].y);
     }
-    if (!leader) shib_move(parent.party[character.party].x, parent.party[character.party].y);
+    // Handle close
+    if (leader && (range > max || range < min || !range)) move_to_coords(leader.real_x + getRndInteger(((min + max)/2) * -1, ((min + max)/2)), leader.real_y + getRndInteger(((min + max)/2) * -1, ((min + max)/2))); else if (!leader) shib_move(parent.party[character.party].x + getRndInteger(((min + max)/2) * -1, ((min + max)/2)), parent.party[character.party].y + getRndInteger(((min + max)/2) * -1, ((min + max)/2)));
+}
+
+//This function will ether move straight towards the target entity,
+//or utilize smart_move to find their way there.
+function move_to_position(position) {
+    if (is_moving(character)) return;
+    move_to_coords(position.x, position.y)
 }
 
 function move_to_coords(x, y) {
@@ -55,17 +51,11 @@ function move_to_coords(x, y) {
     if(can_move_to(x,y)) {
         if (smart.moving) stop();
         move(x,y);
-    } else smart_move({x:x,y:y});
+    }
+    else smart_move({x:x,y:y});
 }
 
-let shibMoveStoredDestination;
 function shib_move(destination, y = undefined) {
-    if (!shibMoveStoredDestination) shibMoveStoredDestination = destination;
-    if (is_moving(character) && shibMoveStoredDestination === destination) return;
-    smart_move(destination, y);
-}
-
-function move_to_position(position) {
     if (is_moving(character)) return;
-    move_to_coords(position.x, position.y)
+    smart_move(destination, y);
 }
