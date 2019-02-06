95be555c59e4acd9b1c71b1e017db3358dd6cc9a
shibdib <supernfadmin@gmail.com>
2019-02-06 06:49:30 -0500

    cleanup merchant

--- 15_merchant.js
+++ 15_merchant.js
@@ -1,41 +1,4 @@
 game_log("---Merchant Script Start---");
 load_code(2);
-//Put monsters you want to kill in here
-//If your character has no target, it will travel to a spawn of the first monster in the list below.
-let state = "farm";
 
-//Movement And Attacking
-setInterval(function () {
-    if (character.party && state === 'farm') farm();
-    if (state === 'resupply_potions') resupply_potions();
-}, 100);//Execute 10 times per second
-
-//Potions and state
-setInterval(function () {
-    state_controller();
-    if (character.hp / character.max_hp < 0.25) {
-        use('use_hp');
-    } else if (character.mp / character.max_mp < 0.75) {
-        use('use_mp');
-    } else if (character.hp / character.max_hp < 0.45) {
-        use('use_hp');
-    }
-}, 500);//Execute 2 times per second
-
-function state_controller() {
-    //If dead respawn
-    if (character.rip) return respawn();
-    //Default to farming
-    let new_state = "farm";
-    //Do we need potions?
-    new_state = potion_check(new_state)
-    //If state changed set it and announce
-    if (state != new_state) {
-        game_log("---NEW STATE " + new_state + "---");
-        state = new_state;
-    }
-}
-
-function farm() {
-    shibMove('town')
-}
+shibMove('town');
