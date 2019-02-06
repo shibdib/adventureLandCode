a6d2b62a6ea6c08ef46bcf239f4c192f3da2a919
shibdib <supernfadmin@gmail.com>
2019-02-05 22:24:35 -0500

    fix BIS code

--- 25_equipment.js
+++ 25_equipment.js
@@ -1,28 +1,58 @@
 // Cycles thru inventory and equips bis
 function equip_best_available() {
-    for (let key in character.items) {
-        let item = character.items[key];
-        if (!item || item === null) continue;
-        let itemInfo = G.items[item.name];
-        // Get currently slotted item
-        let slottedItem = character.slots[itemInfo.type];
-        // If not a slottable item continue
-        if (!slottedItem) continue;
-        // If slot is empty equip
-        if (slottedItem === null) {
-            equip(key)
-            game_log('Equipping ' + item.name);
-            continue;
+    let specialSlots = {
+        'weapon': 'mainhand',
+        'shield': 'offhand',
+        'ring': ['ring1', 'ring2'],
+        'earring': ['earring1', 'earring2']
+    };
+    main:
+        for (let key in character.items) {
+            let item = character.items[key];
+            if (!item || item === null) continue;
+            let itemInfo = G.items[item.name];
+            // Check if slot doesn't match type
+            let itemSlot = itemInfo.type;
+            if (specialSlots[itemInfo.type]) itemSlot = specialSlots[itemInfo.type];
+            // Handle ears and rings
+            if (Array.isArray(itemSlot)) {
+                for (let slot of itemSlot) {
+                    // Get currently slotted item
+                    let slottedItem = character.slots[slot];
+                    // If not a slottable item continue main
+                    if (slottedItem === undefined) continue main;
+                    // If slot is empty equip
+                    else if (slottedItem === null) {
+                        equip(key);
+                        game_log('Equipping ' + item.name + ' in the ' + slot + ' position.');
+                        continue main;
+                    }
+                    // If slotted item is less valuable unequip and equip the new item
+                    else if (G.items[slottedItem.name].g < itemInfo.g) {
+                        unequip(itemInfo.type);
+                        equip(key);
+                        game_log('Equipping ' + item.name + ' in place of ' + slottedItem.name);
+                        continue main;
+                    }
+                }
+            } else {
+                // Get currently slotted item
+                let slottedItem = character.slots[itemSlot];
+                // If not a slottable item continue
+                if (slottedItem === undefined) continue;
+                // If slot is empty equip
+                else if (slottedItem === null) {
+                    equip(key)
+                    game_log('Equipping ' + item.name + ' in the ' + itemSlot + ' position.');
+                }
+                // If slotted item is less valuable unequip and equip the new item
+                else if (G.items[slottedItem.name].g < itemInfo.g) {
+                    unequip(itemInfo.type);
+                    equip(key);
+                    game_log('Equipping ' + item.name + ' in place of ' + slottedItem.name);
+                }
+            }
         }
-        // If slotted item is less valuable unequip and equip the new item
-        if (item_value(slottedItem) < item_value(item)) {
-            game_log(item.name)
-            unequip(itemInfo.type);
-            equip(key);
-            game_log('Equipping ' + item.name + ' in place of ' + slottedItem.name);
-            continue;
-        }
-    }
 }
 
 //Looks for weapon type equipped
