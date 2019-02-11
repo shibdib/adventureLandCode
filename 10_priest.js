a574fed522905483749ee0fd6029bc8d0a46ba2b
shibdib <supernfadmin@gmail.com>
2019-02-10 20:09:07 -0500

    priest should kite when standing still

--- 10_priest.js
+++ 10_priest.js
@@ -75,6 +75,6 @@ function farm() {
         if (lowest_health && lowest_health.health_ratio <= 0.99 && in_attack_range(lowest_health)) {
             heal(lowest_health);
         }
-        if (!combat) moveToLeader(); else moveToLeader(character.range * 0.425, character.range * 0.99);
+        if (!combat) moveToLeader(); else kite();
     }
 }
