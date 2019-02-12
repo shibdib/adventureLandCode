4ab0e10e7b86bed94ca480afe83ef3183b22f6ab
shibdib <supernfadmin@gmail.com>
2019-02-11 20:54:39 -0500

    fix load order

--- 2_requires.js
+++ 2_requires.js
@@ -1,14 +1,13 @@
 game_log('Loading Modules.....');
+load_code(80); // Constants
 load_code(21); // Movement
 load_code(22); // Party management
 load_code(23); // Shopping
 load_code(24); // Combat
 load_code(25); // Equipment
 load_code(26); // Bank
-load_code(80); // Constants
 load_code(96); // Smart Move overhaul
 load_code(97); // GUI
-load_code(98); // References
 load_code(99); // Helpers
 load_code(69); // Update
 load_code(3); // Contribution code
