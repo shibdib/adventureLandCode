61b85e7c3f69084a258d37a9c2ed71213bd5d88c
shibdib <supernfadmin@gmail.com>
2019-02-18 02:53:12 -0500

    remove respawn check

--- 27_targeting.js
+++ 27_targeting.js
@@ -20,8 +20,7 @@ function findBestMonster(minXp, lastTarget, array = false) {
         // Filter out duplicates, then filter out targets based on maxAttack/xp and some other things that cause outliers
         // TODO: add more args to the filter to allow this to find the mini boss esque people (Green jr)
         sorted = sortEntitiesByXp(monsterTypes.filter((v, i, a) => a.indexOf(v) === i)).filter((m) => getMonsterDPS(m, true) < healsPerSecond
-            && G.monsters[m].xp >= xpTarget && (!G.monsters[m].dreturn || G.monsters[m].dreturn <= 95) && !G.monsters[m].stationary && (!G.monsters[m].evasion || G.monsters[m].evasion <= 99)
-            && G.monsters[m].respawn < 120);
+            && G.monsters[m].xp >= xpTarget && (!G.monsters[m].dreturn || G.monsters[m].dreturn <= 95) && !G.monsters[m].stationary && (!G.monsters[m].evasion || G.monsters[m].evasion <= 99));
         if (sorted.length > 4) break;
         // Lower the XP target per loop
         xpTarget *= 0.9;
