d94d15ac71ab013104d00b21ae2b3f43a3ba0dd7
shibdib <supernfadmin@gmail.com>
2019-02-20 16:44:57 -0500

    add easy dev button

--- 97_gui.js
+++ 97_gui.js
@@ -5,6 +5,9 @@ add_top_button(1, 'Update Code', function () {
 add_top_button(2, 'Refresh Party', function () {
     refreshCharacters(true)
 });
+add_top_button(3, 'Dev', function () {
+    parent.require('electron').remote.getCurrentWebContents().openDevTools();
+});
 // Clear Drawings
 setInterval(function () {
     clear_drawings();
