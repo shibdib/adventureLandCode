8e769895ab93a36caaecd680980fd25c7006589b
shibdib <supernfadmin@gmail.com>
2019-02-24 07:05:19 -0500

    market collector start

---
+++ 98_marketCollector.js
@@ -0,0 +1,81 @@
+let apiKey = "";
+
+// Price Check Loop
+let priceDetails = {};
+setInterval(function () {
+    cachePriceInfo();
+    sendMarketData();
+}, 15000);
+
+function cachePriceInfo() {
+    let merchants = Object.values(parent.entities).filter(mob => is_character(mob) && mob.ctype === "merchant" && mob.stand);
+    priceDetails.providerName = character.name;
+    priceDetails.region = parent.server_region;
+    priceDetails.realmID = parent.server_identifier;
+    for (let merchant of merchants) {
+        for (let s = 1; s <= 16; s++) {
+            let slot = merchant.slots['trade' + s];
+            if (slot) {
+                let level = '';
+                if (slot.level) level = slot.level;
+                if (slot.b) {
+                    if (!priceDetails[slot.name + level]) {
+                        priceDetails[slot.name + level] = {
+                            bhigh: slot.price,
+                            bavg: slot.price,
+                            blow: slot.price,
+                            bseen: 1
+                        }
+                    } else {
+                        let details = priceDetails[slot.name + level];
+                        let seen = details.bseen + 1;
+                        let avg = (details.bavg + slot.price) / 2;
+                        let high = details.bhigh;
+                        if (high < slot.price) high = slot.price;
+                        let low = details.blow;
+                        if (low > slot.price) low = slot.price;
+                        details.bhigh = high;
+                        details.bavg = avg;
+                        details.blow = low;
+                        details.bseen = seen;
+                        priceDetails[slot.name + level] = details;
+                    }
+                } else {
+                    if (!priceDetails[slot.name + level]) {
+                        priceDetails[slot.name + level] = {
+                            shigh: slot.price,
+                            savg: slot.price,
+                            slow: slot.price,
+                            sseen: 1
+                        }
+                    } else {
+                        let details = priceDetails[slot.name + level];
+                        let seen = details.sseen + 1;
+                        let avg = (details.savg + slot.price) / 2;
+                        let high = details.shigh;
+                        if (high < slot.price) high = slot.price;
+                        let low = details.slow;
+                        if (low > slot.price) low = slot.price;
+                        details.shigh = high;
+                        details.savg = avg;
+                        details.slow = low;
+                        details.sseen = seen;
+                        priceDetails[slot.name + level] = details;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function sendMarketData() {
+    if (!Object.keys(priceDetails).length) return;
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://almarket.shibdib.info/receiveData");
+    var data = {
+        apiKey: apiKey,
+        data: priceDetails
+    };
+    priceDetails = {};
+    request.send(data);
+}
